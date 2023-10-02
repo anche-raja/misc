8d1a3aaecd687adf97371976bdb7b23ba31cdcd8
Raja Anche <90988778+anche-raja@users.noreply.github.com>
2023-10-02 18:49:39 -0400

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -217,4 +217,39 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
     scale_out_cooldown = 300 # 5 minutes cooldown to scale out
   }
 }
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "ecs-cpu-step-scaling"
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  policy_type        = "StepScaling"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 300
+    metric_aggregation_type = "Average"
+    
+    step_adjustment {
+      scaling_adjustment          = 1
+      metric_interval_lower_bound = 0
+    }
+  }
+}
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "ecs-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "70"
+  alarm_description   = "This metric checks CPU utilization"
+  alarm_actions       = [aws_appautoscaling_policy.ecs_policy.arn] 
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.container_cluster.name
+    ServiceName = aws_ecs_service.container_service.name
+  }
+}
 

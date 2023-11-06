7e63ac938e55745f8b48755cf12b9261d3a99067
Raja Anche <90988778+anche-raja@users.noreply.github.com>
2023-11-06 17:11:24 -0500

    Update s3write.js

--- s3write.js
+++ s3write.js
@@ -1,97 +1,75 @@
 const AWS = require('aws-sdk');
-const zlib = require('zlib');
-const { Readable } = require('stream');
-
-const s3 = new AWS.S3();
-
-exports.handler = async (event) => {
-    // Your data
-    const data = JSON.stringify({
-        key: 'value'
-    });
-
-    // Convert to buffer
-    const buffer = Buffer.from(data, 'utf-8');
-
-    // Gzip the buffer
-    const gzippedBuffer = zlib.gzipSync(buffer);
-
-    // Create a stream from the gzipped buffer
-    const stream = Readable.from(gzippedBuffer);
-
-    // Params for S3 upload
-    const uploadParams = {
-        Bucket: 'your-bucket-name',
-        Key: 'your-key.gz',
-        Body: stream,
-        ContentType: 'application/gzip'
-    };
-
-    try {
-        // Upload gzipped file to S3
-        const uploadResponse = await s3.upload(uploadParams).promise();
-        console.log(`File uploaded successfully at ${uploadResponse.Location}`);
-        return {
-            statusCode: 200,
-            body: JSON.stringify('File saved to S3 successfully!')
-        };
-    } catch (err) {
-        console.log(`An error occurred: ${err}`);
-        return {
-            statusCode: 500,
-            body: JSON.stringify('Failed to save file.')
-        };
-    }
-};
-
-
 const crypto = require('crypto');
 const fs = require('fs');
+const zlib = require('zlib');
+const { promisify } = require('util');
+const stream = require('stream');
 
-// Calculate MD5 hash of the file
-function calculateMD5(filePath) {
-  const fileContent = fs.readFileSync(filePath);
-  const hash = crypto.createHash('md5').update(fileContent).digest('hex');
-  return hash;
-}
+const pipeline = promisify(stream.pipeline);
 
-const localFilePath = '/path/to/your/file.gz';
-const fileMD5 = calculateMD5(localFilePath);
-
-
-
-const AWS = require('aws-sdk');
+// Initialize S3 client
 const s3 = new AWS.S3();
 
-async function uploadFileToS3(bucketName, key, localFilePath, fileMD5) {
-  const fileContent = fs.readFileSync(localFilePath);
+// Function to create a gzip file
+async function createGzipFile(content, filePath) {
+  await pipeline(
+    stream.Readable.from(content),
+    zlib.createGzip(),
+    fs.createWriteStream(filePath)
+  );
+}
+
+// Function to calculate MD5 hash of the file
+function calculateMD5(filePath) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash('md5');
+    const stream = fs.createReadStream(filePath);
+    stream.on('data', data => hash.update(data));
+    stream.on('end', () => resolve(hash.digest('hex')));
+    stream.on('error', reject);
+  });
+}
+
+// Function to upload file to S3
+async function uploadFileToS3(bucketName, key, localFilePath) {
+  const fileStream = fs.createReadStream(localFilePath);
+  const fileMD5 = await calculateMD5(localFilePath);
   const params = {
     Bucket: bucketName,
     Key: key,
-    Body: fileContent,
-    ContentMD5: Buffer.from(fileMD5, 'hex').toString('base64')
+    Body: fileStream,
+    ContentMD5: Buffer.from(fileMD5, 'hex').toString('base64'),
   };
-  
+  return s3.upload(params).promise();
+}
+
+// Your local file and S3 details
+const content = 'Your content to gzip and upload';
+const localFilePath = '/path/to/your/file.gz';
+const bucketName = 'your-bucket-name';
+const s3Key = 'your-file.gz';
+
+// Create a gzip file, calculate MD5, upload to S3, and verify MD5 hash
+async function executeUpload() {
   try {
-    const data = await s3.putObject(params).promise();
-    return data.ETag; // The ETag may or may not be the MD5 hash depending on how S3 stores the file.
+    // Create a gzip file locally
+    await createGzipFile(content, localFilePath);
+
+    // Upload file to S3
+    const s3Response = await uploadFileToS3(bucketName, s3Key, localFilePath);
+
+    // Compare local MD5 hash with the ETag from S3
+    const localFileMD5 = await calculateMD5(localFilePath);
+    const s3ETag = s3Response.ETag.replace(/"/g, ''); // S3 ETag contains quotes
+
+    if (localFileMD5 === s3ETag) {
+      console.log('Success! MD5 hash matches with S3 ETag.');
+    } else {
+      console.warn('Warning: MD5 hash does not match S3 ETag.');
+    }
   } catch (error) {
-    console.error("Error uploading file: ", error);
-    throw error;
+    console.error('An error occurred:', error);
   }
 }
 
-// Call the upload function
-const bucketName = 'your-bucket-name';
-const key = 'your-s3-key-for-the-file.gz';
-uploadFileToS3(bucketName, key, localFilePath, fileMD5).then(s3ETag => {
-  // Compare ETag with local MD5 hash
-  const cleanETag = s3ETag.replace(/"/g, ''); // Remove any quotes from ETag string
-  if (cleanETag === fileMD5) {
-    console.log("File uploaded successfully and MD5 hashes match.");
-  } else {
-    console.log("File uploaded, but MD5 hashes do not match!");
-  }
-});
-
-
+executeUpload();

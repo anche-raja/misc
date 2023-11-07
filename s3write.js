3880dd5900eb781636aa686ee9a1da6d19b8bcfe
Raja Anche <90988778+anche-raja@users.noreply.github.com>
2023-11-07 13:32:54 -0500

    Update s3write.js

--- s3write.js
+++ s3write.js
@@ -99,3 +99,76 @@ const md5Hash = gzipStringToFile(content, outputFilePath);
 // Log the MD5 hash
 console.log("MD5 Hash:", md5Hash);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const AWS = require('aws-sdk');
+const crypto = require('crypto');
+const zlib = require('zlib');
+
+// Initialize the S3 client
+const s3 = new AWS.S3();
+
+// Function to gzip string content and calculate MD5
+function gzipAndHashContent(content) {
+  // Gzip string content synchronously
+  const gzippedContent = zlib.gzipSync(content);
+
+  // Calculate MD5 hash of gzipped content
+  const hash = crypto.createHash('md5').update(gzippedContent).digest('hex');
+
+  // Return gzipped content and its MD5 hash
+  return { gzippedContent, hash };
+}
+
+// Function to upload gzipped content to S3
+function uploadGzipToS3(bucketName, key, gzippedContent, md5Hash) {
+  // Convert the MD5 hash to base64
+  const base64md5 = Buffer.from(md5Hash, 'hex').toString('base64');
+
+  // Set the parameters for the S3 upload
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+    Body: gzippedContent,
+    ContentMD5: base64md5,
+    // Specify the Content-Encoding to indicate that the content is gzipped
+    ContentEncoding: 'gzip',
+  };
+
+  // Upload the gzipped content to S3
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.error('Error uploading to S3:', err);
+    } else {
+      // The ETag may not match the MD5 hash exactly due to how S3 computes it, especially if multipart upload was used
+      console.log('Successfully uploaded to S3:', data);
+      // You may want to compare the MD5 hash manually if strict verification is needed
+    }
+  });
+}
+
+// String content to be gzipped and uploaded
+const contentToGzip = 'Your string content here';
+
+// S3 details
+const bucketName = 'your-bucket-name';
+const key = 'your-object-key.gz';
+
+// Gzip the content and calculate MD5
+const { gzippedContent, hash } = gzipAndHashContent(contentToGzip);
+
+// Upload the gzipped content to S3
+uploadGzipToS3(bucketName, key, gzippedContent, hash);
+

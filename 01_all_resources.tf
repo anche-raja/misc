e5bc046f674380306de8fad765d0205b4f525894
Raja Anche <90988778+anche-raja@users.noreply.github.com>
2024-11-26 16:39:33 -0500

    Update 01_all_resources.tf

--- 01_all_resources.tf
+++ 01_all_resources.tf
@@ -1,3 +1,10 @@
+ASSUME_ROLE_OUTPUT=$(aws sts assume-role --role-arn $ROLE_TO_ASSUME --role-session-name DeleteDynamoDBSession)
+export AWS_ACCESS_KEY_ID=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SecretAccessKey')
+export AWS_SESSION_TOKEN=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SessionToken')
+
+
+
 #########################################################################
 # CREATE DEPENDENCY RESOURCES/INFRASTRUCTURE
 #########################################################################
